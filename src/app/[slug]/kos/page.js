73abd329442081d3a2aa8f86d3fb36c683947388
Satria-Kurniawan <satriakurniawan2001@gmail.com>
2023-03-13 21:53:17 +0800

    updating detail kos

--- src/app/[slug]/kos/page.js
+++ src/app/[slug]/kos/page.js
@@ -1,3 +1,33 @@
+import Image from "next/image";
+
 export default function KosDetail() {
-  return <div>Kos detail</div>;
+  return (
+    <main className="mt-10">
+      <div className="flex h-[70vh]">
+        <Image
+          src={"/assets/koss/1.jpg"}
+          width={1280}
+          height={720}
+          alt="kos detail"
+          className="w-[68vw] rounded-l-lg pr-2"
+        />
+        <div className="flex flex-col w-[32vw]">
+          <Image
+            src={"/assets/koss/1.jpg"}
+            width={1280}
+            height={720}
+            alt="kos detail"
+            className="h-1/2 rounded-tr-lg pb-1"
+          />
+          <Image
+            src={"/assets/koss/1.jpg"}
+            width={1280}
+            height={720}
+            alt="kos detail"
+            className="h-1/2 rounded-br-lg pt-1"
+          />
+        </div>
+      </div>
+    </main>
+  );
 }
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,8 @@
 export default function Button({ isPrimary, text, className = "" }) {
   return (
     <button
-      className={`${
-        isPrimary && "bg-primary"
-      } py-1.5 px-4 rounded-lg ${className}`}
+      className={`${isPrimary && "bg-primary text-white"
+        } py-1.5 px-4 rounded-lg ${className}`}
     >
       {text}
     </button>
